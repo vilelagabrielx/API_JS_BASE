499d0dea091276d7dfa3a11976dca0e0e87f81d2
gabriel feijó vilela <61162949+vilelagabrielx@users.noreply.github.com>
2023-02-11 16:18:56 -0300

    feat: validação de rotas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 const routes = Router();
-
 routes.use('/products', productsRouter);
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello DEV!' });
-});
+// routes.get('/', (request, response) => {
+//   return response.json({ message: 'Hello DEV!' });
+// });
 
 export default routes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
-
+import { errors } from 'celebrate';
 import '@shared/typeorm';
 
 const app = express();
@@ -14,22 +15,28 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(errors());
+
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
+    // Verifica se o erro é uma instância de AppError
     if (error instanceof AppError) {
-      console.log('oi');
+      // Retorna uma resposta com o status code específico para esse erro e uma mensagem de erro personalizada
       return response.status(error.statusCode).json({
         status: 'error',
         message: error.message,
       });
     }
+    // Retorna uma resposta com o status code 500 (Internal Server Error) e uma mensagem genérica de erro
     return response.status(500).json({
       status: 'error',
       message: 'Internal server error',
     });
+    // "next" é usado para passar o controle para o próximo middleware na pilha.
+    //se eu não usa ro next minha aplicação fica presa aqui
   },
 );
 
 app.listen(3333, () => {
-  console.log('server starter on port 3333!');
+  // console.log('server starter on port 3333!');
 });
